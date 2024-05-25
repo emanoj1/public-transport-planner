5b9be2860cd857be1dcc67c59f8d31ba2b0e4ab9
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-05-25 16:30:16 +1000

    Initial code for About page

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a team dedicated to making your daily commute easier.</p>
+    </div>
+  );
+};
+
+export default About;
