0fcb65c05fef1affe76c1fea79e4bd08f308a354
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-05-25 16:30:48 +1000

    Initial code for Contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>You can reach us at contact@commuteplanner.com</p>
+    </div>
+  );
+};
+
+export default Contact;
