86e4ad82f4866b3f2c67add8b848d9eb78b1b420
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-05-30 18:46:38 +1000

    Upload profile photo and update About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import skedgoLogo from '../assets/skedgo_logo_large.webp';
+import manojProfilePic from '../assets/manoj_polaroid_photo.png'
 import '../App.css'; 
 
 
@@ -7,11 +8,14 @@ const About = () => {
   return (
     <div className="content-container">
       <h1>About Me</h1>
+      <img src={manojProfilePic} alt="SkedGo Logo" className="manojProfilePic" />
       <p>Hi there! &#128075; My name is Manoj Kumar, and I am based in Sydney, Australia &#129432;</p> 
       <p>As part of a hackathon initiative from my bootcamp coding course, I built this web app! &#128640;</p>
-      <p>Each student, either working solo or in a group, had the opportunity to create something that utilizes React, NodeJS, JavaScript, and an API. I chose to build a public transport timetable app that helps commuters see their upcoming journey times. It covers select regions across the world and includes various types of public transport like buses, trains, ferries, etc.</p>
-      <p>I regularly use public transport every day, and I will be using this app going forward! &#128653; Slightly partial :)</p>
-      <p>Additionally, this is the first web app I have ever built and deployed (30/5/24), and it's an exciting day for me! 🥹 Ten months ago, I had no idea how to create one.</p>
+      <p>Each student, either working solo or in a group, had the opportunity to create something that utilizes React, NodeJS, JavaScript, and an API. 
+        I chose to build a public transport timetable app that helps commuters plan their departure & arrival for their chosen locations. 
+        You can choose over 400 cities from across the world covering buses, trains, ferries, etc. 
+        As time goes on, I am planning to cover as much of the global transit systems as possible! &#128653; </p>
+      <p>Additionally, this is the first web app I have personally ever built and deployed to the world! The launch day (30/5/24) was an exciting day for me! 🥹 </p>
       <h1>Thank you!</h1>
       <p>A massive shoutout to the folks (Tim, Mauro, and Adrian) at SkedGo, whose TripGo API was used to make this web app. </p>
       <img src={skedgoLogo} alt="SkedGo Logo" className="skedgoLogo" />
