3b08afd58b3599dd635808d8abb4e29ef632c2ce
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-05-30 11:06:50 +1000

    Upload Bus favicon

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,9 @@ const About = () => {
   return (
     <div>
       <h1>About Us</h1>
-      <p>I am building a web app that shows you public transport timetable.</p>
+      <p>Actually, it's more like, "About Me"! 
+        My name is Manoj Kumar, based in Sydney, Australia.
+        As a part of my bootcamp I am building a web app that shows you public transport timetable.</p>
     </div>
   );
 };
