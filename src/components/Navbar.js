40acb4f0907246321ca5d31f7b7784e653f7de1f
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-05-31 14:09:36 +1000

    feat: Hamburger menu for mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react'; // Import useState from react
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../assets/commuter-fyi-logo-2.png';
 
 const Navbar = () => {
-    return (
-      <nav className="navbar">
-        <div className="navbar-wrapper">
-            <Link to="/">
-                <img src={logo} alt="Public Transport Planner Logo" className="navbar-logo" />
-            </Link>
-          <ul className="navbar-links">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/worldtransit">World Transit</Link></li>
-          </ul>
-        </div>
-      </nav>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-wrapper">
+        <Link to="/">
+          <img src={logo} alt="Public Transport Planner Logo" className="navbar-logo" />
+        </Link>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
+          <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+          <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
+          <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
+          <li><Link to="/worldtransit" onClick={toggleMenu}>World Transit</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
   
   export default Navbar;
