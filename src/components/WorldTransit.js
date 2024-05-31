8b665d33a613899df2e782550bfc8a1e96f174aa
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-05-31 15:34:39 +1000

    Add content to World Transit page

--- src/components/WorldTransit.js
+++ src/components/WorldTransit.js
@@ -6,39 +6,54 @@ const WorldTransit = () => {
   return (
     <div className="content-container">
       <h1>Public transport/transit information from around the world.</h1>
-      <p>
-        On this page, you can find weblinks to major public transportation systems from around the world. 
-        We hope this will help you in your travels! If you have any extra info to be added to the page, please let me know!
-      </p>
-      <h1>Australia</h1>
+      <p>On this page, you can find weblinks to major public transportation systems from around the world. We hope this will help you in your travels!</p> 
+      <p>This page is a work-in-progress. If you have any extra info for the page, <a href="https://commuter.fyi/contact" target="_blank" rel="noopener noreferrer">please let me know!</a></p>
+
+      <h2>Australia 🇦🇺</h2> 
       
-      <p>Queensland https://translink.com.au/</p>
-      <p>Victoria https://www.ptv.vic.gov.au/</p>
-      <p>NSW https://transportnsw.info/</p>
-      <p>South Australia https://www.adelaidemetro.com.au/</p>
-      <p>Queensland https://www.transperth.wa.gov.au/</p>
-      <p>Tasmania https://www.metrotas.com.au/</p>
-      <p>ACT https://www.transport.act.gov.au/</p>
-      <p>Northern Territory https://nt.gov.au/</p>
+      <p><a href="https://translink.com.au/" target="_blank" rel="noopener noreferrer">Queensland</a></p>
+      <p><a href="https://www.ptv.vic.gov.au/" target="_blank" rel="noopener noreferrer">Victoria</a></p>
+      <p><a href="https://transportnsw.info/" target="_blank" rel="noopener noreferrer">New South Wales</a></p>
+      <p><a href="https://www.adelaidemetro.com.au/" target="_blank" rel="noopener noreferrer">South Australia</a></p>
+      <p><a href="https://www.transperth.wa.gov.au/" target="_blank" rel="noopener noreferrer">Western Australia</a></p>
+      <p><a href="https://www.metrotas.com.au/" target="_blank" rel="noopener noreferrer">Tasmania</a></p>
+      <p><a href="https://www.transport.act.gov.au/" target="_blank" rel="noopener noreferrer">Australian Capital Territory (ACT)</a></p>
+      <p><a href="https://nt.gov.au/" target="_blank" rel="noopener noreferrer">Northern Territory (NT)</a></p>
 
-      <h1>New Zealand</h1>
-      <p>https://www.nzta.govt.nz/walking-cycling-and-public-transport/public-transport/travelling-by-public-transport/</p>
-      <p>https://www.intercity.co.nz/</p>
+      <h2>Great Britain 🇬🇧</h2>
+      <p><a href="https://www.visitbritain.com/en/plan-your-trip/travelling-around-britain" target="_blank" rel="noopener noreferrer">All transport info</a></p>
 
-      <h1>United Kingdom/Great Britain</h1>
-      <p>https://www.visitbritain.com/en/plan-your-trip/travelling-around-britain</p>
+      <h2>France 🇫🇷</h2>
+      <p><a href="https://www.ratp.fr/en" target="_blank" rel="noopener noreferrer">RATP - main website with all info</a></p>
+      <p><a href="https://www.ratp.fr/en/horaires-metro" target="_blank" rel="noopener noreferrer">Metro timetable</a></p>
+      <p><a href="https://www.ratp.fr/en/horaires-bus" target="_blank" rel="noopener noreferrer">Bus timetable</a></p>
+      <p><a href="https://www.ratp.fr/en/horaires-tramway" target="_blank" rel="noopener noreferrer">Trams timetable</a></p>
+      <p><a href="https://www.ratp.fr/en/horaires-rer" target="_blank" rel="noopener noreferrer">RER (Rapid Transit System) timetable</a></p>
 
-      <h1>USA</h1>
-      <p>https://www.visitbritain.com/en/plan-your-trip/travelling-around-britain</p>
+      <h2>Indonesia 🇮🇩</h2>
+      <p><a href="https://transjakarta.co.id/peta-rute/" target="_blank" rel="noopener noreferrer">TransJakarta</a></p>
+      <p><a href="https://commuterline.id/perjalanan-krl/jadwal-kereta" target="_blank" rel="noopener noreferrer">KAI Commuter Rail (Jakarta, Tangerang, Depok, Bogor, and Bekasi.</a></p>
+      <p><a href="https://jakartamrt.co.id/en" target="_blank" rel="noopener noreferrer">MRT Jakarta</a></p>
 
-      <h1>France</h1>
-      <p>https://www.ratp.fr/en</p>
+      <h2>Japan 🇯🇵</h2>
+      <p><a href="https://www.jreast.co.jp/multi/en/" target="_blank" rel="noopener noreferrer">East Japan Railway Company</a></p>
+      <p><a href="https://www.jrhokkaido.co.jp/global/index.html" target="_blank" rel="noopener noreferrer">Hokkaido Railway Company</a></p>
+      <p><a href="https://www.jrkyushu.co.jp/english/" target="_blank" rel="noopener noreferrer">Kyushu Railway Company</a></p>
+      <p><a href="https://www.jr-shikoku.co.jp/global/en/" target="_blank" rel="noopener noreferrer">Shikoku Railway Company</a></p>
+      <p><a href="https://www.westjr.co.jp/global/en/" target="_blank" rel="noopener noreferrer">West Japan Railway Company</a></p>
+      <p><a href="https://global.jr-central.co.jp/en/" target="_blank" rel="noopener noreferrer">Central Japan Railway Company</a></p>
 
-      <h1>Japan</h1>
-      <p>https://www.visitbritain.com/en/plan-your-trip/travelling-around-britain</p>
+      <h2>New Zealand 🇳🇿</h2>
+      <p><a href="https://www.nzta.govt.nz/walking-cycling-and-public-transport/public-transport/travelling-by-public-transport/" target="_blank" rel="noopener noreferrer">New Zealand Transport Agency</a></p>
+      <p><a href="https://www.intercity.co.nz/" target="_blank" rel="noopener noreferrer">Intercity</a></p>
 
-      <h1>United Kingdom</h1>
-      <p>https://www.visitbritain.com/en/plan-your-trip/travelling-around-britain</p>
+      <h2>Singapore 🇸🇬</h2>
+      <p><a href="https://www.lta.gov.sg/content/ltagov/en/getting_around/public_transport/rail_network.html" target="_blank" rel="noopener noreferrer">Rail Network</a></p>
+      <p><a href="https://www.lta.gov.sg/content/ltagov/en/getting_around/public_transport/bus_network.html" target="_blank" rel="noopener noreferrer">Bus Network</a></p>
+      <p><a href="https://www.simplygo.com.sg/card-collection-faq" target="_blank" rel="noopener noreferrer">SimplyGo travel card</a></p>
+
+      <h2>USA 🇺🇸</h2>
+      <p><a href="https://www.apta.com/research-technical-resources/public-transportation-links/" target="_blank" rel="noopener noreferrer">Link to transit agencies across USA</a></p>
 
     </div>
   );
